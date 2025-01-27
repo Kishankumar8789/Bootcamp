0d96e89cd3ef991ce04e583347674d5c3f9d099d
Kishan Kumar <163028205+Kishankumar8789@users.noreply.github.com>
2025-01-27 20:21:17 +0530

    Add files via upload

---
+++ sumtwo_num.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter num a&b =");
+    scanf("%d %d", &a,&b);
+    c = a+b;
+    printf("sum of %d and %d = %d", a,b,c);
+    getch();
+}
