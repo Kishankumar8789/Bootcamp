2c3043eb02e4a90e65942aa91d42da4bebdb8904
Kishankumar8789 <kishankumar9771392942@gmail.com>
2025-01-29 10:53:12 +0530

    Add File

---
+++ largest_three_no.c
@@ -0,0 +1,41 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int num1 = 100;
+//     int num2 = 20;
+//     int num3 = 1500;
+//     if(num1 > num2){
+//         printf("Num1 is greater");
+//     }
+//     else if(num2 > num3){
+//         printf("Num2 is greater");
+//     }
+//     else if(num1 < num3){
+//         printf("num3 is greater");
+//     }
+//     else{
+//         printf("wrong");
+//     }
+//     return 0;
+// }
+
+#include<stdio.h>
+int main()
+{
+    int num1 = 100;
+    int num2 = 20;
+    int num3 = 1500;
+    if(num1 > num2){
+        printf("Num1 is greater");
+    }
+    else if(num2 > num3){
+        printf("Num2 is greater");
+    }
+    else if(num1 < num3){
+        printf("num3 is greater");
+    }
+    else{
+        printf("wrong");
+    }
+    return 0;
+}
